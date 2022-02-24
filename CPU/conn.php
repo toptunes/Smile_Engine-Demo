c03d481202c06bbf93aa959646635df2b149bd3d
moein <smileenginesoft@gmail.com>
2022-02-25 00:56:07 +0330

    ...

--- CPU/conn.php
+++ CPU/conn.php
@@ -4,46 +4,6 @@ if (!defined('24647ETW$@^&@%@%*#'))
     die('error 24');
 }
 
-class jjson
-{
-    public static function jsonToDebug($jsonText = '')
-    {
-        $arr = json_decode($jsonText, true);
-        $html = "";
-        if ($arr && is_array($arr))
-        {
-            $html .= self::_arrayToHtmlTableRecursive($arr);
-        }
-        return $html;
-    }
-
-    private static function _arrayToHtmlTableRecursive($arr)
-    {
-        $str = "<table><tbody>";
-        foreach ($arr as $key => $val)
-        {
-            $str .= "<tr>";
-            $str .= "<td>$key</td>";
-            $str .= "<td>";
-            if (is_array($val))
-            {
-                if (!empty($val))
-                {
-                    $str .= self::_arrayToHtmlTableRecursive($val);
-                }
-            }
-            else
-            {
-                $str .= "<strong>$val</strong>";
-            }
-            $str .= "</td></tr>";
-        }
-        $str .= "</tbody></table>";
-
-        return $str;
-    }
-
-}
 
 class conn
 {
