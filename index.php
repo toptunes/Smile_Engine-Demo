c9e9b9ad189694070fd98320edf2e70779fb2fe9
moein <smileenginesoft@gmail.com>
2022-02-26 01:49:00 +0330

    ....

--- index.php
+++ index.php
@@ -3,62 +3,27 @@
 
    readfile("app/lib/meta.html");
    ?>
-<style>
-   <?php
-      //  Pay attention! some "include" files will replace on release version
-
-      include ("./app/styles/toptunes.ir_app_1.css");
-      $code = ob_get_clean();
-      echo str_replace(array(
-          "\r",
-          "\n"
-      ) , '', $code);
-      
-      //  Pay attention! some "include" files will replace on release version
-      ob_start();
-      include "./app/styles/mainpage.css.php";
-      $code = ob_get_clean();
-      echo str_replace(array(
-          "\r",
-          "\n"
-      ) , '', $code);
-      
-      ?>
-</style>
+<link type="text/css" rel="stylesheet" href="/app/styles/toptunes.ir_app_1.css" />
+<link type="text/css" rel="stylesheet" href="/app/styles/custom.css" />
 <script>
-   <?php
-      //include "./app/scripts/js_core.php";
-      
-      ?>
-    <?php
-      //  Pay attention! some "include" files will replace on release version
-      echo file_get_contents("./app/scripts/smi_lee.js");
-  
-      
-      ?>
+    if(/MSIE/i['test'](navigator['userAgent'])==true||/rv/i['test'](navigator['userAgent'])==true||/Edge/i['test'](navigator['userAgent'])==true){
+            alert("Please Update your browser! / مرورگر خود را بروز رسانی کنید");
+   }
 </script>
-<!--  <link rel="stylesheet" type="text/css" href="app/styles/toptunes.ir_app_1.css" />-->
 </head>
 <body>
    <keep-alive id="app">
       <router-view v-bind:app="app">
       </router-view>
    </keep-alive>
-   <?php
-      /*
-        <div id="dialogs">
-         <dab-alert v-bind:app="app"></dab-alert>
-         <dab-toasts v-bind:app="app"></dab-toasts>
-        </div> 
-      */
-      ?>
    <div id="error_found"></div>
    <div id="dab-sidebar-overlay"></div>
+   <script src="/app/scripts/smi_lee.js"></script>
    <script src="/app/scripts/nids.js"></script>
-   <script src="/app/scripts/app.js.php?42"></script>
+   <script src="/app/scripts/app.js.php"></script>
    <?php
-      include "./app/scripts/threejs_codes.php";
-      ?>
+      //include "./app/scripts/threejs_codes.php";
+   ?>
    <script>
       jalaliDatepicker.startWatch({
           separatorChar: "/",
@@ -68,22 +33,26 @@
           showTodayBtn: true,
           showEmptyBtn: true
       });
-      
    </script>
-   <script>
-      setTimeout("errorload()", 10000);
-      function errorload(){
+<script>
       
-      var dear_nav_el = document.getElementById("dear_nav").innetHTML;
-      
-      if(typeof dear_nav_el == 'undefined'){
-      
-      document.getElementById("error_found").innetHTML = "<p>اگر سایت برای شما نمایش داده نمیشود . صفحه را ریفرش کنید یا مرورگر خود را آپدیت کنید . مرورگر پیشنهادی گوگل کروم یا فایر فاکس است</p>";
-      }
-      
-      
-      }
-      
+         setTimeout(errorload, 10000 );
+        
+         function errorload(){
+            
+         var dear_nav_el=document.getElementById("dear_nav").innerText;
+         var err_f = document.getElementById("error_found");
+            if(typeof dear_nav_el == 'undefined'){
+         
+               err_f.innerText = "Please Update your browser!";
+   
+            }else{
+   
+               err_f.innerText = "";
+   
+            }
+         }
+         
    </script>
 </body>
 </html>
