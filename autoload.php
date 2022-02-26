a21adfc1ba055a95252240bfa484d97af76b5a49
moein <smileenginesoft@gmail.com>
2022-02-26 14:10:05 +0330

    ...

--- autoload.php
+++ autoload.php
@@ -4,6 +4,5 @@ spl_autoload_register(function ($class_name) {
     include __DIR__."/CPU/".$class_name . '.php';
 });
 
-include __DIR__."app/views/html/switcher/routing.php";
-include __DIR__."app/views/html/switcher/render_element_patterns.php";
+
 ?>
