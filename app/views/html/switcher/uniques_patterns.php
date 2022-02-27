4bebfb776cacb8d4e5ea9f643a7c4023968f1fe9
moein <smileenginesoft@gmail.com>
2022-02-28 00:19:15 +0330

    ...

--- app/views/html/switcher/uniques_patterns.php
+++ app/views/html/switcher/uniques_patterns.php
@@ -12,15 +12,15 @@ $insert_data = json_decode($this->data_string);
 
 $uniques_patterns = [
 
-"limit_specifications"  => "`product_id` = \''.$insert_data->product_id.'\' and `spec_value_string` = \''.$insert_data->spec_value_string.'\' and `spec_key` = \''.$insert_data->spec_key.'\'  and `users_id` = \''.$this->user_id.'\' ",
+"limit_specifications"  => "`product_id` = '$insert_data->product_id' and `spec_value_string` = '$insert_data->spec_value_string' and `spec_key` = '$insert_data->spec_key'  and `users_id` = '$this->user_id' ",
 "limit_specifications_msg"  => "مشابه این ویژگی را قبلا اضافه کردید و میتوانید در جدول پایین صفحه ویرایش کنید",
 
-"add_to_all_stores"  => "`color_picked` = \''.$insert_data->color_picked.'\' and `product_id` = \''.$insert_data->product_id.'\' and `users_id` = \''.$this->user_id.'\'",
+"add_to_all_stores"  => "`color_picked` = '$insert_data->color_picked' and `product_id` = '$insert_data->product_id' and `users_id` = '$this->user_id' ",
 
 "add_to_all_stores_msg"  => "شما {{0}} عدد از این محصول با این رنگ قبلا اضافه کرده اید <span  class=\"off-percent text-light m-1 rounded-pill mx-2 \" style=\"width: 40px;float:left; display: inline-flex;height: 40px; background:{{1}}\"></span><p class=\"m-2\">و در سبد خرید شما موجود است</p>",
 "add_to_all_stores_cols"  => "how_many_wanted;color_picked",
 
-"add_to_all_stores_NOCOLOR"  => "`color_picked` = \''.$insert_data->color_picked.'\' and `product_id` = \''.$insert_data->product_id.'\' and `users_id` = \''.$this->user_id.'\'",
+"add_to_all_stores_NOCOLOR"  => "`color_picked` = '$insert_data->color_picked' and `product_id` =   '$insert_data->product_id' and `users_id` = '$this->user_id' ",
 
 "add_to_all_stores_NOCOLOR_msg"  => "شما {{0}} عدد از این محصول قبلا اضافه کرده اید <span  class=\"off-percent text-light m-1 rounded-pill mx-2 \" style=\"width: 40px;float:left; display: inline-flex;height: 40px; background:{{1}}\"></span><p class=\"m-2\">و در سبد خرید شما موجود است</p>",
 "add_to_all_stores_NOCOLOR_cols"  => "how_many_wanted;color_picked"
