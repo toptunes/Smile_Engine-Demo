39ac840c92bce4f29e39e8b5940da9f3713fb4a0
Mohammad (dehaz) Norouzi <smileenginesoft@gmail.com>
2022-03-25 17:03:04 +0430

    ...

--- app/views/html/mohammad_norouzi_app.php
+++ app/views/html/mohammad_norouzi_app.php
@@ -1,13 +1,12 @@
 <?php
+
+/*
 header('Pragma: public');
 header('Cache-Control: max-age=386400');
 header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + 386400));
+*/
 
 
-/*
-  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");*/
 ?>
 <div id="cover"></div>
 <div class="mm-page mm-slideout" id="mm-0">
